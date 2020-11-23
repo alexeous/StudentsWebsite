ee98b1820bceae5ef521e7a94d6c063ef4320dcd
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-23 18:30:37 +0700

    Add IRepository and BaseRepository types

---
+++ StudentsWebsite.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using StudentsWebsite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories
+{
+    class BaseRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private UniversityDbContext dbContext;
+
+        public BaseRepository(UniversityDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task InsertAsync(T obj)
+        {
+            dbContext.Set<T>().Add(obj);
+            return dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ StudentsWebsite.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using StudentsWebsite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories
+{
+    public interface IRepository<T> where T : class, IEntity
+    {
+        Task InsertAsync(T obj);
+    }
+}
