b500874074d44dd5ac7b1efe32ca533e99d5ce7d
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-23 18:37:10 +0700

    Implement repository interfaces

---
+++ StudentsWebsite.DataAccess/Repositories/Impl/StudentVisitingTeacherRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentsWebsite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories.Impl
+{
+    class StudentVisitingTeacherRepository : BaseRepository<StudentVisitingTeacher>
+    {
+        public StudentVisitingTeacherRepository(UniversityDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ StudentsWebsite.DataAccess/Repositories/Impl/TeacherRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentsWebsite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories.Impl
+{
+    class TeacherRepository : BaseRepository<Teacher>
+    {
+        public TeacherRepository(UniversityDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
