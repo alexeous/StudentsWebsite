48de4394326d986073ec28b4fd72c35647cedc4f
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-23 14:48:59 +0700

    Add model classes

---
+++ Models/Mark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebsite.Models
+{
+    public class Mark
+    {
+        public int Id { get; set; }
+
+        public int MarkValue { get; set; }
+    }
+}
---
+++ Models/StudentVisitingTeacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebsite.Models
+{
+    public class StudentVisitingTeacher
+    {
+        public int Id { get; set; }
+
+        public Student Student { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+        public List<Mark> Marks { get; set; }
+    }
+}
