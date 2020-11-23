48047288c943ed75904349d789a019dfee9d9df8
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-23 17:18:20 +0700

    Create UniversityDbContext class

---
+++ StudentsWebsite.DataAccess/Repositories/UniversityDbContext.cs
@@ -0,0 +1,28 @@
+﻿using StudentsWebsite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories
+{
+    class UniversityDbContext : DbContext
+    {
+        private const string ConnectionStringName = "DbConnection";
+
+        public UniversityDbContext() 
+            : base(ConnectionStringName)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DbSet<StudentVisitingTeacher> StudentsVisitingTeachers { get; set; }
+
+        public DbSet<Mark> Marks { get; set; }
+    }
+}
