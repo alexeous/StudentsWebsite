aa4c0078be002b8fa115ce0f27a2a3337752216f
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-23 18:33:04 +0700

    Add repository interfaces for all models

---
+++ StudentsWebsite.DataAccess/Repositories/IStudentVisitingTeacherRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudentsWebsite.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.DataAccess.Repositories
+{
+    public interface IStudentVisitingTeacherRepository : IRepository<StudentVisitingTeacher>
+    {
+    }
+}
