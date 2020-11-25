214b90f4d4a11a9efeb1f201e02d3f21137fccaf
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-25 12:42:19 +0700

    Add Composition project with Autofac installed

---
+++ StudentsWebsite.Composition/CompositionRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.Composition
+{
+    public static class CompositionRoot
+    {
+        public static void RegisterDependencies()
+        {
+
+        }
+    }
+}
