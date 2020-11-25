56b29005938a556533b8c1f1578ed9e225db63c6
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-25 12:47:38 +0700

    Register controllers in CompositionRoot

--- StudentsWebsite.Composition/CompositionRoot.cs
+++ StudentsWebsite.Composition/CompositionRoot.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace StudentsWebsite.Composition
 {
     public static class CompositionRoot
     {
-        public static void RegisterDependencies()
+        public static void RegisterDependencies(Assembly mvcApplicationAssembly)
         {
+            var builder = new ContainerBuilder();
 
+            builder.RegisterControllers(mvcApplicationAssembly);
+
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(container);
         }
     }
 }
