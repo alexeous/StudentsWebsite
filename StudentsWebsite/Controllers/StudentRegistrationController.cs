26d7778636d931bec3587b6726c9190806060ffd
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 15:14:30 +0700

    Sketch student registration form

---
+++ StudentsWebsite/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,26 @@
+﻿using StudentsWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StudentsWebsite.Controllers
+{
+    public class StudentRegistrationController : ApiController
+    {
+        public async Task<IHttpActionResult> Post([FromBody] StudentRegistrationModel value)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(value);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ StudentsWebsite/Models/StudentRegistrationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebsite.Models
+{
+    public class StudentRegistrationModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Password confirmation is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
