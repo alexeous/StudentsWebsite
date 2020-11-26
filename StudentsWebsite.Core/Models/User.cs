4822dea353aa9851f60beb24773d4159ab034a29
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 11:34:51 +0700

    Rename User's 'Username' property to 'Email'

--- StudentsWebsite.Core/Models/User.cs
+++ StudentsWebsite.Core/Models/User.cs
@@ -12,7 +12,7 @@ namespace StudentsWebsite.Core.Models
 
         public string LastName { get; set; }
 
-        public string Username { get; set; }
+        public string Email { get; set; }
 
         public string Password { get; set; }
     }
