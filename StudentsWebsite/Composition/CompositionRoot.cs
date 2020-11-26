8b5b6c31f2b1283d47864f4736fbd4f1c0f7c4a9
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 16:15:21 +0700

    Register registration services in Autofac

--- StudentsWebsite/Composition/CompositionRoot.cs
+++ StudentsWebsite/Composition/CompositionRoot.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using StudentsWebsite.Composition.Modules;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace StudentsWebsite.Composition
@@ -17,12 +19,20 @@ namespace StudentsWebsite.Composition
         {
             var builder = new ContainerBuilder();
 
-            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            var config = GlobalConfiguration.Configuration;
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterWebApiFilterProvider(config);
+            builder.RegisterWebApiModelBinderProvider();
+
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
 
             builder.RegisterModule(new UniversityDbContextModule());
             builder.RegisterModule(new RepositoryModule());
+            builder.RegisterModule(new RegistrationModule());
 
             var container = builder.Build();
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
---
+++ StudentsWebsite/Composition/Modules/RegistrationModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using StudentsWebsite.Business.Registration;
+using StudentsWebsite.Business.Registration.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebsite.Composition.Modules
+{
+    class RegistrationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserRegistrationService>().As<IUserRegistrationService>().InstancePerRequest();
+            builder.RegisterType<StudentRegistrationService>().As<IStudentRegistrationService>().InstancePerRequest();
+            builder.RegisterType<TeacherRegistrationService>().As<ITeacherRegistrationService>().InstancePerRequest();
+        }
+    }
+}
