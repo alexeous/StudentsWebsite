4b637fe007dafa9eb23ce8e113c1f02a6c80e90c
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 14:05:51 +0700

    Add empty registration views

---
+++ StudentsWebsite/Controllers/RegistrationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentsWebsite.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public ActionResult RegisterStudent()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterTeacher()
+        {
+            return View();
+        }
+    }
+}
