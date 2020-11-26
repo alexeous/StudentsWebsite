0ceb97281686307ae4b8604ca23ac326f7967d1b
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 18:42:25 +0700

    Implement teacher registration

---
+++ StudentsWebsite.Business/Registration/InvalidSecretWordException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.Business.Registration
+{
+
+    [Serializable]
+    public class InvalidSecretWordException : Exception
+    {
+        public InvalidSecretWordException() { }
+        public InvalidSecretWordException(string message) : base(message) { }
+        public InvalidSecretWordException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidSecretWordException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ StudentsWebsite/Controllers/TeacherRegistrationApiController.cs
@@ -0,0 +1,59 @@
+﻿using StudentsWebsite.Business.Registration;
+using StudentsWebsite.Core.Models;
+using StudentsWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace StudentsWebsite.Controllers
+{
+    [Route("api/register/teacher")]
+    public class TeacherRegistrationApiController : ApiController
+    {
+        private ITeacherRegistrationService teacherRegistration;
+
+        public TeacherRegistrationApiController(ITeacherRegistrationService teacherRegistration)
+        {
+            this.teacherRegistration = teacherRegistration;
+        }
+
+        public async Task<IHttpActionResult> Post([FromBody] TeacherRegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var teacher = new Teacher
+                {
+                    Subject = model.Subject,
+                    User = new User
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        Password = model.Password
+                    }
+                };
+                try
+                {
+                    await teacherRegistration.Register(teacher, model.SecretWord);
+                }
+                catch (UserExistsException)
+                {
+                    return BadRequest("This email address has already been registered.");
+                }
+                catch (InvalidSecretWordException)
+                {
+                    return BadRequest("Invalid secret word.");
+                }
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
