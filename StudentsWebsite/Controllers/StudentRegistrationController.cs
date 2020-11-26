0408b76d1d135d264c113875e32a206cbb66bca3
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 16:32:15 +0700

    Implement student registration form

--- StudentsWebsite/Controllers/StudentRegistrationController.cs
+++ StudentsWebsite/Controllers/StudentRegistrationController.cs
@@ -1,4 +1,6 @@
-﻿using StudentsWebsite.Models;
+﻿using StudentsWebsite.Business.Registration;
+using StudentsWebsite.Core.Models;
+using StudentsWebsite.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +13,36 @@ namespace StudentsWebsite.Controllers
 {
     public class StudentRegistrationController : ApiController
     {
+        private IStudentRegistrationService studentRegistration;
+
+        public StudentRegistrationController(IStudentRegistrationService studentRegistration)
+        {
+            this.studentRegistration = studentRegistration;
+        }
+
         public async Task<IHttpActionResult> Post([FromBody] StudentRegistrationModel value)
         {
             if (ModelState.IsValid)
             {
-                return Ok(value);
+                var student = new Student
+                {
+                    User = new User
+                    {
+                        FirstName = value.FirstName,
+                        LastName = value.LastName,
+                        Email = value.Email,
+                        Password = value.Password
+                    }
+                };
+                try
+                {
+                    await studentRegistration.Register(student);
+                }
+                catch (UserExistsException)
+                {
+                    return BadRequest("This email address has already been registered.");
+                }
+                return Ok();
             }
             else
             {
