324a893f0ce1d89ddd61971de7386ee75be2d7ff
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 17:54:07 +0700

    Add authentication service

---
+++ StudentsWebsite.Business/Authentication/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using StudentsWebsite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.Business.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<User> Authenticate(string email, string password);
+    }
+}
---
+++ StudentsWebsite.Business/Authentication/Impl/AuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using StudentsWebsite.Business.Password;
+using StudentsWebsite.Core.Models;
+using StudentsWebsite.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsWebsite.Business.Authentication.Impl
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private IUserRepository userRepository;
+        private IPasswordService passwordService;
+
+        public AuthenticationService(IUserRepository userRepository, IPasswordService passwordService)
+        {
+            this.userRepository = userRepository;
+            this.passwordService = passwordService;
+        }
+
+        public async Task<User> Authenticate(string email, string password)
+        {
+            User user = await userRepository.GetByEmailAsync(email);
+            if (passwordService.VerifyPassword(password, user.Password))
+            {
+                return user;
+            }
+            return null;
+        }
+    }
+}
