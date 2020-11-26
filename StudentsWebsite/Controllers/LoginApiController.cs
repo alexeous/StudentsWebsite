42f7b6cbab375b4e68f486a73b173ced8049eb8f
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-26 18:15:40 +0700

    Add login api controller and login model

---
+++ StudentsWebsite/Controllers/LoginApiController.cs
@@ -0,0 +1,42 @@
+﻿using StudentsWebsite.Business.Authentication;
+using StudentsWebsite.Core.Models;
+using StudentsWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace StudentsWebsite.Controllers
+{
+    [Route("api/login")]
+    public class LoginApiController : ApiController
+    {
+        private IAuthenticationService authentication;
+
+        public LoginApiController(IAuthenticationService authentication)
+        {
+            this.authentication = authentication;
+        }
+
+        public async Task<IHttpActionResult> Post([FromBody] LoginModel value)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await authentication.Authenticate(value.Email, value.Password);
+                if (user != null)
+                {
+                    FormsAuthentication.SetAuthCookie(user.Email, true);
+                    return Ok(new { FirstName = user.FirstName, LastName = user.LastName });
+                }
+                else {
+                    return Unauthorized();
+                }
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
