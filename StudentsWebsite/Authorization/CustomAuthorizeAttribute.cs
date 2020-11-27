6f61cf5846e80239432f77fdfc10c0914a0993a9
Alexey Bykov <alexey-bycov@yandex.ru>
2020-11-27 14:26:01 +0700

    CustomAuthorizationAttribute

---
+++ StudentsWebsite/Authorization/CustomAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using StudentsWebsite.Business.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace StudentsWebsite.Authorization
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        private Role[] allowedRoles;
+
+        public CustomAuthorizeAttribute(params Role[] allowedRoles)
+        {
+            this.allowedRoles = allowedRoles;
+        }
+
+        public override async Task OnAuthorizationAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            IAuthorizationService authorization = GetAuthorizationService(actionContext);
+            string email = actionContext.RequestContext.Principal.Identity.Name;
+            if (!await authorization.AuthorizeAsync(email, allowedRoles))
+            {
+                HandleUnauthorizedRequest(actionContext);
+            }
+        }
+
+        private static IAuthorizationService GetAuthorizationService(HttpActionContext actionContext)
+        {
+            return (IAuthorizationService)actionContext.RequestContext
+                .Configuration.DependencyResolver.GetService(typeof(IAuthorizationService));
+        }
+    }
+}
---
+++ StudentsWebsite/Composition/Modules/AuthorizationModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using StudentsWebsite.Business.Authorization;
+using StudentsWebsite.Business.Authorization.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsWebsite.Composition.Modules
+{
+    class AuthorizationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AuthorizationService>().As<IAuthorizationService>().InstancePerLifetimeScope();
+        }
+    }
+}
